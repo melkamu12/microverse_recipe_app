2fa3298f13daa4fdc1f7cd31c2ae2fb66d8dbd26
Melkamu Almawu Ayen <malmawu@gmail.com>
2023-10-27 22:03:30 +0300

    rspec testing and fixing sign out issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     devise_for :users
-    root 'foods#index'
+    root 'home#index'
     resources :foods, only: [:index, :show, :new, :create, :destroy]
 
     resources :recipes do
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
         end
         resources :recipe_foods, only: [:new, :create, :destroy]
     end
-    resources :users, only: [:index, :show, :new, :create]
     resources :public_recipes, only: [:index]
     resources :shopping_lists, only: [:index] do
         collection do
